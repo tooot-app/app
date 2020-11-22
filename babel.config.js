6d6b808af217d17f9e13e205e294de2221c48f22
Zhiyuan Zheng <me@xmflsct.com>
2020-11-22 01:13:13 +0100

    Removed unused packages

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,7 @@
 module.exports = function (api) {
   api.cache(true)
   return {
-    presets: [
-      'babel-preset-expo',
-      // {
-      //   runtime: 'automatic',
-      //   development: process.env.NODE_ENV === 'development',
-      //   importSource: '@welldone-software/why-did-you-render'
-      // }
-    ],
+    presets: ['babel-preset-expo'],
     plugins: [
       ['@babel/plugin-proposal-optional-chaining'],
       [
