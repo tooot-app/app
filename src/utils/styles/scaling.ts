8a49c92c79476c9383583b4a2eb15d95a8431d53
Zhiyuan Zheng <me@xmflsct.com>
2021-03-10 14:00:16 +0100

    Update adaptive size not dependent on screen size

--- src/utils/styles/scaling.ts
+++ src/utils/styles/scaling.ts
@@ -7,7 +7,9 @@ const guidelineBaseWidth = 375
 
 const scale = (size: number) => (width / guidelineBaseWidth) * size
 // const verticalScale = (size: number) => (height / guidelineBaseHeight) * size
+// const adaptiveScale = (size: number, factor: number = 0) =>
+//   size + (scale(size) - size) * factor
 const adaptiveScale = (size: number, factor: number = 0) =>
-  size + (scale(size) - size) * factor
+  size + size * (factor / 8)
 
 export { adaptiveScale }
