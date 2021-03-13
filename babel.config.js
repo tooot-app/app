32a7d23dc97e74f723e40f2874e24673b4454a4c
Zhiyuan Zheng <me@xmflsct.com>
2021-03-13 18:27:44 +0100

    Fixed #61

--- babel.config.js
+++ babel.config.js
@@ -1,24 +1,34 @@
 module.exports = function (api) {
   api.cache(true)
+
+  const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './assets',
+          '@root': './src',
+          '@api': './src/api',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils'
+        }
+      }
+    ],
+    'react-native-reanimated/plugin'
+  ]
+
+  if (
+    process.env.NODE_ENV === 'production' ||
+    process.env.BABEL_ENV === 'production'
+  ) {
+    plugins.push('transform-remove-console')
+  }
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-proposal-optional-chaining',
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@assets': './assets',
-            '@root': './src',
-            '@api': './src/api',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@utils': './src/utils'
-          }
-        }
-      ],
-      'react-native-reanimated/plugin'
-    ]
+    plugins
   }
 }
