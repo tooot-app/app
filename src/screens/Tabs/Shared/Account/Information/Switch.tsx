c4ec88609e12781da51fdab2cbd2c7dc1f808c7f
Zhiyuan Zheng <me@xmflsct.com>
2021-03-29 00:22:30 +0200

    Using Crowdin now

--- src/screens/Tabs/Shared/Account/Information/Switch.tsx
+++ src/screens/Tabs/Shared/Account/Information/Switch.tsx
@@ -5,12 +5,12 @@ import { useTranslation } from 'react-i18next'
 
 const AccountInformationSwitch: React.FC = () => {
   const navigation = useNavigation()
-  const { t } = useTranslation()
+  const { t } = useTranslation('screenTabs')
 
   return (
     <Button
       type='text'
-      content={t('meSwitch:heading')}
+      content={t('me.stacks.switch.name')}
       onPress={() => navigation.navigate('Tab-Me-Switch')}
     />
   )
