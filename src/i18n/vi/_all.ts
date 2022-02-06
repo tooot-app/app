7bf428f1796fde07e7b87b1c8e683c1b6cee8c9f
Zhiyuan Zheng <me@xmflsct.com>
2022-02-06 23:25:16 +0100

    Added support for KO and VI

---
+++ src/i18n/vi/_all.ts
@@ -0,0 +1,17 @@
+export default {
+  common: require('./common'),
+
+  screens: require('./screens'),
+  screenActions: require('./screens/actions'),
+  screenAnnouncements: require('./screens/announcements'),
+  screenCompose: require('./screens/compose'),
+  screenImageViewer: require('./screens/imageViewer'),
+  screenTabs: require('./screens/tabs'),
+
+  componentInstance: require('./components/instance'),
+  componentMediaSelector: require('./components/mediaSelector'),
+  componentParse: require('./components/parse'),
+  componentRelationship: require('./components/relationship'),
+  componentRelativeTime: require('./components/relativeTime'),
+  componentTimeline: require('./components/timeline')
+}
