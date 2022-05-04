b2043d884fe1014872d04f96dc3ba431f34916ab
Zhiyuan Zheng <me@xmflsct.com>
2022-05-04 23:55:46 +0200

    Update MainActivity.java
    https://docs.swmansion.com/react-native-gesture-handler/docs/guides/migrating-off-rnghenabledroot/

--- android/app/src/main/java/com/xmflsct/app/tooot/MainActivity.java
+++ android/app/src/main/java/com/xmflsct/app/tooot/MainActivity.java
@@ -4,7 +4,6 @@ import android.os.Bundle;
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.ReactRootView;
-import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 import expo.modules.ReactActivityDelegateWrapper;
 
@@ -21,18 +20,5 @@ public class MainActivity extends ReactActivity {
     @Override
     protected String getMainComponentName() {
         return "main";
-    }
-
-    @Override
-    protected ReactActivityDelegate createReactActivityDelegate() {
-        return new ReactActivityDelegateWrapper(
-            this,
-            new ReactActivityDelegate(this, getMainComponentName()) {
-                @Override
-                protected ReactRootView createRootView() {
-                    return new RNGestureHandlerEnabledRootView(MainActivity.this);
-                }
-            }
-        );
     }
 }
