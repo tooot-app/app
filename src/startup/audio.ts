1d6790c76ef9873b7078d6eca8fc22630ace12aa
Zhiyuan Zheng <me@xmflsct.com>
2022-05-08 22:55:39 +0200

    Bump rest of the packages

--- src/startup/audio.ts
+++ src/startup/audio.ts
@@ -1,12 +1,12 @@
-import { Audio } from 'expo-av'
+import { Audio, InterruptionModeAndroid, InterruptionModeIOS } from 'expo-av'
 import log from './log'
 
 const audio = () => {
   log('log', 'audio', 'setting audio playback default options')
   Audio.setAudioModeAsync({
     playsInSilentModeIOS: true,
-    interruptionModeIOS: Audio.INTERRUPTION_MODE_IOS_DUCK_OTHERS,
-    interruptionModeAndroid: Audio.INTERRUPTION_MODE_ANDROID_DUCK_OTHERS
+    interruptionModeIOS: InterruptionModeIOS.DuckOthers,
+    interruptionModeAndroid: InterruptionModeAndroid.DuckOthers
   })
 }
 
