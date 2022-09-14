969d4abe0e2e161d469399c0fcf68d23a7c0b42e
xmflsct <me@xmflsct.com>
2022-09-14 21:52:16 +0200

    Move image viewer to a new plugin

--- src/screens/ImagesViewer.tsx
+++ src/screens/ImagesViewer.tsx
@@ -1,4 +1,5 @@
 import analytics from '@components/analytics'
+import GracefullyImage from '@components/GracefullyImage'
 import { HeaderCenter, HeaderLeft, HeaderRight } from '@components/Header'
 import { Message } from '@components/Message'
 import { useActionSheet } from '@expo/react-native-action-sheet'
@@ -6,14 +7,23 @@ import { RootStackScreenProps } from '@utils/navigation/navigators'
 import { useTheme } from '@utils/styles/ThemeManager'
 import React, { useCallback, useRef, useState } from 'react'
 import { useTranslation } from 'react-i18next'
-import { Platform, Share, StatusBar, View } from 'react-native'
-import FlashMessage from 'react-native-flash-message'
 import {
-  SafeAreaProvider,
-  useSafeAreaInsets
-} from 'react-native-safe-area-context'
-import ImageViewer from './ImageViewer/Root'
+  Dimensions,
+  FlatList,
+  PixelRatio,
+  Platform,
+  Share,
+  StatusBar,
+  View,
+  ViewToken
+} from 'react-native'
+import FlashMessage from 'react-native-flash-message'
+import { LongPressGestureHandler } from 'react-native-gesture-handler'
+import { Zoom, createZoomListComponent } from 'react-native-reanimated-zoom'
+import { SafeAreaProvider, useSafeAreaInsets } from 'react-native-safe-area-context'
 import saveImage from './ImageViewer/save'
+
+const ZoomFlatList = createZoomListComponent(FlatList)
 
 const ScreenImagesViewer = ({
   route: {
@@ -26,6 +36,9 @@ const ScreenImagesViewer = ({
     return null
   }
 
+  const SCREEN_WIDTH = Dimensions.get('screen').width
+  const SCREEN_HEIGHT = Dimensions.get('screen').height
+
   const insets = useSafeAreaInsets()
 
   const { mode, theme } = useTheme()
@@ -34,6 +47,7 @@ const ScreenImagesViewer = ({
   const initialIndex = imageUrls.findIndex(image => image.id === id)
   const [currentIndex, setCurrentIndex] = useState(initialIndex)
 
+  const listRef = useRef<FlatList>(null)
   const messageRef = useRef<FlashMessage>(null)
 
   const { showActionSheetWithOptions } = useActionSheet()
@@ -71,15 +85,84 @@ const ScreenImagesViewer = ({
     )
   }, [currentIndex])
 
+  const renderItem = React.useCallback(
+    ({
+      item
+    }: {
+      item: RootStackScreenProps<'Screen-ImagesViewer'>['route']['params']['imageUrls'][0]
+    }) => {
+      const screenRatio = SCREEN_WIDTH / SCREEN_HEIGHT
+      const imageRatio = item.width && item.height ? item.width / item.height : 1
+
+      const maxWidthScale = item.width ? (item.width / SCREEN_WIDTH / PixelRatio.get()) * 4 : 0
+      const maxHeightScale = item.height ? (item.height / SCREEN_WIDTH / PixelRatio.get()) * 4 : 0
+      const max = Math.max.apply(Math, [maxWidthScale, maxHeightScale, 4])
+
+      return (
+        <Zoom
+          maximumZoomScale={max > 8 ? 8 : max}
+          children={
+            <View
+              style={{
+                width: SCREEN_WIDTH,
+                height: SCREEN_HEIGHT,
+                flexDirection: 'row',
+                alignItems: 'center',
+                justifyContent: 'center'
+              }}
+            >
+              <GracefullyImage
+                uri={{ preview: item.preview_url, remote: item.remote_url, original: item.url }}
+                blurhash={item.blurhash}
+                dimension={{
+                  width:
+                    screenRatio > imageRatio ? (item.height || 100) / screenRatio : SCREEN_WIDTH,
+                  height:
+                    screenRatio > imageRatio ? SCREEN_HEIGHT : (item.width || 100) * screenRatio
+                }}
+              />
+            </View>
+          }
+        />
+      )
+    },
+    []
+  )
+
+  const onViewableItemsChanged = useCallback(
+    ({ viewableItems }: { viewableItems: ViewToken[] }) => {
+      setCurrentIndex(viewableItems[0].index || 0)
+    },
+    []
+  )
+
   return (
-    <SafeAreaProvider>
+    <SafeAreaProvider style={{ backgroundColor: 'black' }}>
       <StatusBar hidden />
-      <ImageViewer
-        images={imageUrls}
-        imageIndex={initialIndex}
-        onImageIndexChange={index => setCurrentIndex(index)}
-        onRequestClose={() => navigation.goBack()}
-        onLongPress={() => {
+      <View
+        style={{
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          alignItems: 'center',
+          marginTop: insets.top,
+          position: 'absolute',
+          width: '100%',
+          zIndex: 999
+        }}
+      >
+        <HeaderLeft content='X' native={false} background onPress={() => navigation.goBack()} />
+        <HeaderCenter inverted content={`${currentIndex + 1} / ${imageUrls.length}`} />
+        <HeaderRight
+          accessibilityLabel={t('content.actions.accessibilityLabel')}
+          accessibilityHint={t('content.actions.accessibilityHint')}
+          content='MoreHorizontal'
+          native={false}
+          background
+          onPress={onPress}
+        />
+      </View>
+      <LongPressGestureHandler
+        onEnded={() => {
           analytics('imageviewer_more_press')
           showActionSheetWithOptions(
             {
@@ -118,37 +201,26 @@ const ScreenImagesViewer = ({
             }
           )
         }}
-        HeaderComponent={() => (
-          <View
-            style={{
-              flex: 1,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-              alignItems: 'center',
-              marginTop: insets.top
-            }}
-          >
-            <HeaderLeft
-              content='X'
-              native={false}
-              background
-              onPress={() => navigation.goBack()}
-            />
-            <HeaderCenter
-              inverted
-              content={`${currentIndex + 1} / ${imageUrls.length}`}
-            />
-            <HeaderRight
-              accessibilityLabel={t('content.actions.accessibilityLabel')}
-              accessibilityHint={t('content.actions.accessibilityHint')}
-              content='MoreHorizontal'
-              native={false}
-              background
-              onPress={onPress}
-            />
-          </View>
-        )}
-      />
+      >
+        <ZoomFlatList
+          ref={listRef}
+          data={imageUrls}
+          pagingEnabled
+          horizontal
+          keyExtractor={item => item.id}
+          renderItem={renderItem}
+          onViewableItemsChanged={onViewableItemsChanged}
+          viewabilityConfig={{
+            itemVisiblePercentThreshold: 50
+          }}
+          initialScrollIndex={initialIndex}
+          getItemLayout={(_, index) => ({
+            length: SCREEN_WIDTH,
+            offset: SCREEN_WIDTH * index,
+            index
+          })}
+        />
+      </LongPressGestureHandler>
       <Message ref={messageRef} />
     </SafeAreaProvider>
   )
