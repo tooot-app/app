398f37d59912391e6cfa852965210e084f44ced8
xmflsct <me@xmflsct.com>
2022-09-24 13:42:15 +0200

    Remove sentry-expo
    Too many dependencies it needs, especially the unused expo-updates

--- src/startup/sentry.ts
+++ src/startup/sentry.ts
@@ -1,12 +1,18 @@
-import * as Sentry from 'sentry-expo'
+import * as Sentry from "@sentry/react-native";
+import { isDevelopment } from "@utils/checkEnvironment";
 import log from './log'
 
 const sentry = () => {
   log('log', 'Sentry', 'initializing')
   Sentry.init({
-    // @ts-ignore
+    enabled: !isDevelopment,
     dsn: 'https://53348b60ff844d52886e90251b3a5f41@o917354.ingest.sentry.io/6410576',
-    enableInExpoDevelopment: false,
+    tracesSampleRate: 0.35,
+    integrations: [
+      new Sentry.ReactNativeTracing({
+        tracingOrigins: ["tooot.app"],
+      }),
+    ],
     autoSessionTracking: true
   })
 }
