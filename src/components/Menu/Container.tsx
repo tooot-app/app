7bbc11e423e5b3735f2ab19ca59de16ac942a596
Jesus Cova <jesuscovam@gmail.com>
2022-11-11 12:03:41 -0500

    added increased hitSlot

--- src/components/Menu/Container.tsx
+++ src/components/Menu/Container.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { StyleSheet, View, Platform } from 'react-native'
 import { StyleConstants } from '@utils/styles/constants'
 
 export interface Props {
   children: React.ReactNode
 }
 
+
 const MenuContainer: React.FC<Props> = ({ children }) => {
-  return <View style={styles.base}>{children}</View>
+  return <View hitSlop={{ top: 10, bottom: 10, left: 10, right: 10 }} style={styles.base}>{children}</View>
 }
 
 const styles = StyleSheet.create({
