6ec9e8bdf4fa5f0b32c47d6a7920ca37b6d6a578
Jesus Cova <jesuscovam@gmail.com>
2022-11-11 12:07:14 -0500

    removed unused import

--- src/components/Menu/Container.tsx
+++ src/components/Menu/Container.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { StyleSheet, View, Platform } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import { StyleConstants } from '@utils/styles/constants'
 
 export interface Props {
