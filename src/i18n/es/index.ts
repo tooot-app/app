1b2e651117df388b7779b6726d4895c91e13434e
xmflsct <me@xmflsct.com>
2022-11-17 20:02:56 +0100

    Added Spanish translation

---
+++ src/i18n/es/index.ts
@@ -0,0 +1,18 @@
+export default {
+  common: require('./common'),
+
+  screens: require('./screens'),
+  screenActions: require('./screens/actions'),
+  screenAnnouncements: require('./screens/announcements'),
+  screenCompose: require('./screens/compose'),
+  screenImageViewer: require('./screens/imageViewer'),
+  screenTabs: require('./screens/tabs'),
+
+  componentContextMenu: require('./components/contextMenu'),
+  componentEmojis: require('./components/emojis'),
+  componentInstance: require('./components/instance'),
+  componentMediaSelector: require('./components/mediaSelector'),
+  componentParse: require('./components/parse'),
+  componentRelationship: require('./components/relationship'),
+  componentTimeline: require('./components/timeline')
+}
