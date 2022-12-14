c4316804fc26003f06907aa39513755d7eaa0125
xmflsct <me@xmflsct.com>
2022-12-15 00:16:13 +0100

    Added 🇺🇦 Slava Ukraini

--- src/i18n/i18n.ts
+++ src/i18n/i18n.ts
@@ -12,6 +12,7 @@ import ko from '@root/i18n/ko'
 import nl from '@root/i18n/nl'
 import pt_BR from '@root/i18n/pt_BR'
 import sv from '@root/i18n/sv'
+import uk from '@root/i18n/uk'
 import vi from '@root/i18n/vi'
 import zh_Hans from '@root/i18n/zh-Hans'
 import zh_Hant from '@root/i18n/zh-Hant'
@@ -31,6 +32,7 @@ import '@formatjs/intl-pluralrules/locale-data/ko'
 import '@formatjs/intl-pluralrules/locale-data/nl'
 import '@formatjs/intl-pluralrules/locale-data/pt'
 import '@formatjs/intl-pluralrules/locale-data/sv'
+import '@formatjs/intl-pluralrules/locale-data/uk'
 import '@formatjs/intl-pluralrules/locale-data/vi'
 import '@formatjs/intl-pluralrules/locale-data/zh'
 
@@ -46,6 +48,7 @@ import '@formatjs/intl-numberformat/locale-data/ko'
 import '@formatjs/intl-numberformat/locale-data/nl'
 import '@formatjs/intl-numberformat/locale-data/pt'
 import '@formatjs/intl-numberformat/locale-data/sv'
+import '@formatjs/intl-numberformat/locale-data/uk'
 import '@formatjs/intl-numberformat/locale-data/vi'
 import '@formatjs/intl-numberformat/locale-data/zh-Hans'
 import '@formatjs/intl-numberformat/locale-data/zh-Hant'
@@ -62,6 +65,7 @@ import '@formatjs/intl-datetimeformat/locale-data/ko'
 import '@formatjs/intl-datetimeformat/locale-data/nl'
 import '@formatjs/intl-datetimeformat/locale-data/pt'
 import '@formatjs/intl-datetimeformat/locale-data/sv'
+import '@formatjs/intl-datetimeformat/locale-data/uk'
 import '@formatjs/intl-datetimeformat/locale-data/vi'
 import '@formatjs/intl-datetimeformat/locale-data/zh-Hans'
 import '@formatjs/intl-datetimeformat/locale-data/zh-Hant'
@@ -79,6 +83,7 @@ import '@formatjs/intl-relativetimeformat/locale-data/ko'
 import '@formatjs/intl-relativetimeformat/locale-data/nl'
 import '@formatjs/intl-relativetimeformat/locale-data/pt'
 import '@formatjs/intl-relativetimeformat/locale-data/sv'
+import '@formatjs/intl-relativetimeformat/locale-data/uk'
 import '@formatjs/intl-relativetimeformat/locale-data/vi'
 import '@formatjs/intl-relativetimeformat/locale-data/zh-Hans'
 import '@formatjs/intl-relativetimeformat/locale-data/zh-Hant'
@@ -102,6 +107,7 @@ i18n.use(initReactI18next).init({
     nl,
     'pt-BR': pt_BR,
     sv,
+    uk,
     vi,
     'zh-Hans': zh_Hans,
     'zh-Hant': zh_Hant
