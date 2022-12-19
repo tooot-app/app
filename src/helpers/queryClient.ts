ca629befc93e7fb1db13d6ff9ebd83f68aac4158
xmflsct <me@xmflsct.com>
2022-12-20 00:45:53 +0100

    Experiment support of Pleroma (#588)

--- src/helpers/queryClient.ts
+++ src/helpers/queryClient.ts
@@ -1,5 +1,21 @@
 import { QueryClient } from '@tanstack/react-query'
 
-const queryClient = new QueryClient({ defaultOptions: { queries: { staleTime: 1000 * 60 * 5 } } })
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      staleTime: 1000 * 60 * 5,
+      retry: (failureCount, error: any) => {
+        if (error?.status === 404) {
+          return false
+        }
+        if (failureCount <= 3) {
+          return true
+        } else {
+          return false
+        }
+      }
+    }
+  }
+})
 
 export default queryClient
