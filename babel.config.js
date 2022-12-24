21d6baa70de1ca07c5397e1ba1a68817c7c51ae2
xmflsct <me@xmflsct.com>
2022-12-24 14:46:39 +0100

    Fix babel

--- babel.config.js
+++ babel.config.js
@@ -1,30 +1,29 @@
 module.exports = function (api) {
   api.cache(false)
 
+  const plugins = [
+    '@babel/plugin-proposal-optional-chaining',
+    [
+      'module-resolver',
+      {
+        root: ['./'],
+        alias: {
+          '@assets': './assets',
+          '@root': './src',
+          '@api': './src/api',
+          '@helpers': './src/helpers',
+          '@components': './src/components',
+          '@screens': './src/screens',
+          '@utils': './src/utils'
+        }
+      }
+    ],
+    'react-native-reanimated/plugin'
+  ]
+
   if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
     plugins.push('transform-remove-console')
   }
 
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      '@babel/plugin-proposal-optional-chaining',
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          alias: {
-            '@assets': './assets',
-            '@root': './src',
-            '@api': './src/api',
-            '@helpers': './src/helpers',
-            '@components': './src/components',
-            '@screens': './src/screens',
-            '@utils': './src/utils'
-          }
-        }
-      ],
-      'react-native-reanimated/plugin'
-    ]
-  }
+  return { presets: ['babel-preset-expo'], plugins }
 }
