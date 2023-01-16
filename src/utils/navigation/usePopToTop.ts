c2a180f4f5bd072fd145c65fec90c944d896fe5e
xmflsct <me@xmflsct.com>
2023-01-16 22:36:49 +0100

    Fix popToTop dev warning

--- src/utils/navigation/usePopToTop.ts
+++ src/utils/navigation/usePopToTop.ts
@@ -1,15 +1,18 @@
-import { StackActions, useFocusEffect, useNavigation } from '@react-navigation/native'
+import { StackActions } from '@react-navigation/native'
 import { useGlobalStorage } from '@utils/storage/actions'
 import { useEffect } from 'react'
+import navigationRef from './navigationRef'
 
 // Mostly used when switching account and sub pages were still querying the old instance
 
-const usePopToTop = () => {
-  const navigation = useNavigation()
+const usePopToTop = (name: string) => {
   const [accountActive] = useGlobalStorage.string('account.active')
 
   useEffect(() => {
-    navigation.dispatch(StackActions.popToTop())
+    const currentRoute = navigationRef.getCurrentRoute()
+    if (currentRoute && currentRoute.name !== name) {
+      navigationRef.dispatch(StackActions.popToTop())
+    }
   }, [accountActive])
 }
 
