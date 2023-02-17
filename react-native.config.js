9b0f07fbf1f55fc8e98fce93f77752292c05c92f
xmflsct <me@xmflsct.com>
2023-02-17 14:22:54 +0100

    Some clean up

--- react-native.config.js
+++ react-native.config.js
@@ -4,6 +4,16 @@ module.exports = {
       platforms: {
         ios: null
       }
+    },
+    '@react-native-menu/menu': {
+      platforms: {
+        ios: null
+      }
+    },
+    'react-native-ios-context-menu': {
+      platforms: {
+        android: null
+      }
     }
   }
 }
