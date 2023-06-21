3154540bba5d6f6370f94c022597542ebfe47548
xmflsct <me@xmflsct.com>
2023-06-21 23:05:40 +0200

    Bump packages

--- babel.config.js
+++ babel.config.js
@@ -1,26 +1,27 @@
 module.exports = function (api) {
   api.cache(false)
 
-  const plugins = [
-    '@babel/plugin-proposal-optional-chaining',
-    [
-      'module-resolver',
-      {
-        root: ['./'],
-        alias: {
-          '@components': './src/components',
-          '@i18n': './src/i18n',
-          '@screens': './src/screens',
-          '@utils': './src/utils'
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      '@babel/plugin-proposal-optional-chaining',
+      [
+        'module-resolver',
+        {
+          root: ['./'],
+          alias: {
+            '@components': './src/components',
+            '@i18n': './src/i18n',
+            '@screens': './src/screens',
+            '@utils': './src/utils'
+          }
         }
-      }
-    ],
-    'react-native-reanimated/plugin'
-  ]
-
-  if (process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production') {
-    plugins.push('transform-remove-console')
+      ]
+    ].concat(
+      process.env.NODE_ENV === 'production' || process.env.BABEL_ENV === 'production'
+        ? ['transform-remove-console']
+        : [],
+      ['react-native-reanimated/plugin']
+    )
   }
-
-  return { presets: ['babel-preset-expo'], plugins }
 }
